566033583d14ae786a2c9bd41067f3afb9035841
aysun imanova <aysunimanova49@gmail.com>
2024-05-25 10:53:22 +0400

    creating new branch

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello my develop branch!");
+        System.out.print("Hello my develop branch!");
     }
 }
